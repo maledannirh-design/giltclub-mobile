9d0c19aa19ff85ca7ce2f1154a1cdcd41f873782
maledannirh-design <male.dannirh@gmail.com>
2026-02-18 13:56:10 +0800

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+    apiKey: "AIzaSyAI2Xn2CbfE9m78n8uDGzccr9mron0scaI",
+    authDomain: "giltianappsmobile.firebaseapp.com",
+    projectId: "giltianappsmobile",
+    storageBucket: "giltianappsmobile.firebasestorage.app",
+    messagingSenderId: "611921553199",
+    appId: "1:611921553199:web:319a304afc1f1e903e63da"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
