be4c8cf1fa10ef0ccc72b051679dab4da6d07203
maledannirh-design <male.dannirh@gmail.com>
2026-02-18 13:39:46 +0800

    Create profile.js

---
+++ app/profile.js
@@ -0,0 +1,8 @@
+export function renderProfile(){
+  const content = document.getElementById("content");
+
+  content.innerHTML = `
+    <h2>Profile</h2>
+    <p>Social identity coming soon...</p>
+  `;
+}
