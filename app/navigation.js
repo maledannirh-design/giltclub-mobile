868f274759dac1992dea06b0f20d55aa2a9f8873
maledannirh-design <male.dannirh@gmail.com>
2026-02-18 13:43:02 +0800

    Update navigation.js

--- app/navigation.js
+++ app/navigation.js
@@ -1,1 +1,27 @@
+export function navigate(page){
+
+  const content = document.getElementById("content");
+
+  switch(page){
+
+    case "home":
+      content.innerHTML = "<h2>Home</h2>";
+      break;
+
+    case "profile":
+      import("./profile.js").then(m=>m.renderProfile());
+      break;
+
+    case "booking":
+      import("./booking.js").then(m=>m.renderBooking());
+      break;
+
+    case "cinema":
+      import("./cinema.js").then(m=>m.renderCinema());
+      break;
+
+    default:
+      content.innerHTML = "<h2>Page not found</h2>";
+  }
+}
 
