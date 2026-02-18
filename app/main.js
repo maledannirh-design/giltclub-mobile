3737f0d364671127836b1526f292059e03ea6ad7
maledannirh-design <male.dannirh@gmail.com>
2026-02-18 13:42:49 +0800

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,1 +1,17 @@
+import "./navigation.js";
+import "./auth.js";
+import "./profile.js";
+import "./booking.js";
+import "./cinema.js";
+import "./social.js";
+
+window.navigate = function(page){
+  import("./navigation.js").then(mod=>{
+    mod.navigate(page);
+  });
+};
+
+document.addEventListener("DOMContentLoaded",()=>{
+  navigate("home");
+});
 
