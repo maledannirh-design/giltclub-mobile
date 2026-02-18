db1b5fd18cafdda152af38483313c599025ab6eb
maledannirh-design <male.dannirh@gmail.com>
2026-02-18 15:29:03 +0800

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,18 +1,7 @@
 import { navigate } from "./navigation.js";
-import { watchAuth } from "./auth.js";
 
 window.navigate = navigate;
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("App loaded");
-
-  watchAuth((user) => {
-    if (user) {
-      console.log("User logged in:", user.uid);
-    } else {
-      console.log("No user logged in");
-    }
-  });
-
   navigate("home");
 });
