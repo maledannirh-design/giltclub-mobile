c643bc0a425a9dee9acbdeaa42f646c9ee9eabfa
maledannirh-design <male.dannirh@gmail.com>
2026-02-18 13:42:32 +0800

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,1 +1,16 @@
+// Firebase init placeholder
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 
