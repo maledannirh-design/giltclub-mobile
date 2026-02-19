3d7e2d644e0c877bb8856c70d75b97af477a6cc0
maledannirh-design <male.dannirh@gmail.com>
2026-02-19 18:04:45 +0800

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,5 +1,9 @@
 export const CANCEL_POLICY = {
-  fullRefundHours: 48,
-  halfRefundStart: 36,
-  halfRefundEnd: 48
+  deadlineHours: 3, // tidak bisa cancel kalau kurang dari ini
+
+  tiers: [
+    { minHoursBefore: 48, refundRate: 1.0 },
+    { minHoursBefore: 36, refundRate: 0.5 },
+    { minHoursBefore: 0,  refundRate: 0.0 }
+  ]
 };
