26fdd13b870b81a7ad4bf29b9c07585754c2f57d
maledannirh-design <male.dannirh@gmail.com>
2026-02-19 17:29:46 +0800

    Update dashboard.js

--- app/dashboard.js
+++ app/dashboard.js
@@ -7,9 +7,8 @@ import {
   orderBy,
   limit,
   where
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { db } from "./firebase.js";
 
 
 // ============================
