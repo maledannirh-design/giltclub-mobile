a4b0b249abafe98a2db921caf21cfbfc140d365f
maledannirh-design <male.dannirh@gmail.com>
2026-02-19 17:44:48 +0800

    Update utils.js

--- app/utils.js
+++ app/utils.js
@@ -1,3 +1,12 @@
 export function formatDate(date){
   return new Date(date).toLocaleDateString("id-ID");
 }
+
+export async function safeExecute(fn){
+  try {
+    return await fn();
+  } catch(e){
+    console.error(e);
+    showToast("System error");
+  }
+}
