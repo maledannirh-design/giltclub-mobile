8fd111efded0f3d94739f6e5d5c8486044af8c93
maledannirh-design <male.dannirh@gmail.com>
2026-02-19 17:38:01 +0800

    Create firestore.js

---
+++ app/firestore.js
@@ -0,0 +1,21 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  where
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  where
+};
