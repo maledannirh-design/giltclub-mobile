7069e1958e02ab1071f4afb7865a94c361c513b7
maledannirh-design <male.dannirh@gmail.com>
2026-02-20 20:57:44 +0800

    Create storage.js

---
+++ app/storage.js
@@ -0,0 +1,11 @@
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+export {
+  ref,
+  uploadBytes,
+  getDownloadURL
+};
