7caf0719d29fb4f934c88d0c23c4952ee8bb75e9
maledannirh-design <male.dannirh@gmail.com>
2026-02-20 20:40:08 +0800

    Update profile.js

--- app/profile.js
+++ app/profile.js
@@ -186,6 +186,20 @@ export async function renderMembers(){
                 onclick="toggleFriend('${uid}')">
                 Add Friend
               </button>
+             <button class="chat-btn"
+  onclick="handleChat('${uid}')">
+  <svg xmlns="http://www.w3.org/2000/svg"
+       width="16"
+       height="16"
+       viewBox="0 0 24 24"
+       fill="none"
+       stroke="currentColor"
+       stroke-width="2"
+       stroke-linecap="round"
+       stroke-linejoin="round">
+    <path d="M21 15a4 4 0 0 1-4 4H7l-4 4V5a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4z"/>
+  </svg>
+</button>
             </div>
 
           </div>
