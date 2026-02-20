308bba39f44455e071e16e88da937f6c1372f688
maledannirh-design <male.dannirh@gmail.com>
2026-02-20 21:44:55 +0800

    Update profile.js

--- app/profile.js
+++ app/profile.js
@@ -220,28 +220,27 @@ export async function renderMembers(){
               Status: ${data.status || "active"}
             </div>
 
-            <div class="member-actions">
-              <button class="follow-btn"
-                onclick="toggleFollow('${uid}')">
-                Follow
-              </button>
-<button class="chat-btn"
-  onclick="handleChat('${uid}')">
-  <svg xmlns="http://www.w3.org/2000/svg"
-       width="16"
-       height="16"
-       viewBox="0 0 24 24"
-       fill="none"
-       stroke="currentColor"
-       stroke-width="2"
-       stroke-linecap="round"
-       stroke-linejoin="round">
-    <path d="M21 15a4 4 0 0 1-4 4H7l-4 4V5a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4z"/>
-  </svg>
-</button>
+          <div class="member-actions">
+  <button class="follow-btn"
+    onclick="toggleFollow('${uid}')">
+    Follow
+  </button>
+
+  <button class="chat-btn"
+    onclick="handleChat('${uid}')">
+    <svg xmlns="http://www.w3.org/2000/svg"
+         width="16"
+         height="16"
+         viewBox="0 0 24 24"
+         fill="none"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round">
+      <path d="M21 15a4 4 0 0 1-4 4H7l-4 4V5a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4z"/>
+    </svg>
+  </button>
 </div>
-</button>
-            </div>
 
           </div>
 
