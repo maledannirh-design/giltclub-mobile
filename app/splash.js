40f65d6c93a38f189ea5b78be706dafb0b5ed29e
maledannirh-design <male.dannirh@gmail.com>
2026-02-22 01:31:53 +0800

    Update splash.js

--- app/splash.js
+++ app/splash.js
@@ -1,4 +1,31 @@
-<div class="splash-screen">
-   <img class="splash-logo">
-   <div id="splashText"></div>
-</div>
+document.addEventListener("DOMContentLoaded", () => {
+  runSplash();
+});
+
+function runSplash(){
+
+  const text = "Where passion, confidence & elegance meet court";
+  const splashText = document.getElementById("splashText");
+  const splashScreen = document.getElementById("splashScreen");
+
+  let index = 0;
+
+  function typeWriter(){
+    if(index < text.length){
+      splashText.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeWriter, 35);
+    } else {
+      setTimeout(() => {
+        splashScreen.classList.add("hide");
+
+        setTimeout(() => {
+          splashScreen.remove();
+        }, 600);
+
+      }, 900);
+    }
+  }
+
+  typeWriter();
+}
