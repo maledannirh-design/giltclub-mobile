4a111f4e962ae1b9ae9ee3b322251860eed37a6f
maledannirh-design <male.dannirh@gmail.com>
2026-02-22 18:34:35 +0800

    Create reward.js

---
+++ app/reward.js
@@ -0,0 +1,11 @@
+export function renderReward(){
+
+  const content = document.getElementById("content");
+
+  content.innerHTML = `
+    <div style="padding:20px;">
+      <h2>Reward</h2>
+      <div>Fitur Tukar Point segera hadir.</div>
+    </div>
+  `;
+}
