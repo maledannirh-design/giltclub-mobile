67cd83f567bc77af539b7bb91303d990f977a830
maledannirh-design <male.dannirh@gmail.com>
2026-02-22 19:31:12 +0800

    Update migrasi.js

--- app/migrasi.js
+++ app/migrasi.js
@@ -1,14 +1,39 @@
 import { recalculateUserStats } from "./userStats.js";
 
-const stats = recalculateUserStats({
-  totalTopup: item.totalTopup,
-  totalPayment: item.totalPayment,
-  membership: userData.membership
-});
+async function migrateUsers(dataList){
 
-await updateDoc(userRef, {
-  level: stats.level,
-  exp: stats.expTotal,
-  gPoint: stats.gPoint
-});
+  for(const item of dataList){
 
+    const snap = await getDocs(
+      query(collection(db,"users"), where("username","==", item.username))
+    );
+
+    if(snap.empty) continue;
+
+    const userDoc = snap.docs[0];
+    const uid = userDoc.id;
+    const userData = userDoc.data();
+
+    const totalPayment = item.totalTopup - item.walletBalance;
+
+    const stats = recalculateUserStats({
+      totalTopup: item.totalTopup,
+      totalPayment: totalPayment,
+      membership: userData.membership
+    });
+
+    await updateDoc(doc(db,"users",uid),{
+      walletBalance: item.walletBalance,
+      totalTopup: item.totalTopup,
+      totalPayment: totalPayment,
+      totalSession: item.totalSession,
+
+      level: stats.level,
+      exp: stats.expTotal,
+      gPoint: stats.gPoint
+    });
+
+  }
+
+  console.log("Migrasi selesai");
+}
