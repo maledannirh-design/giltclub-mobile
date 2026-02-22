c18f0df72a27d0d095e37dd6364a01bd6d18e7ba
maledannirh-design <male.dannirh@gmail.com>
2026-02-22 20:44:32 +0800

    Update migration.js

--- app/migration.js
+++ app/migration.js
@@ -12,19 +12,89 @@ import { recalculateUserStats } from "./userStats.js";
 
 const migrationData = [
   {
-   username: "aderia",
-   totalTopup: 500000,
-   walletBalance: 80000
+   username: "usewin ameera",
+   totalTopup: 1650000,
+   walletBalance: 20250
  },
  {
-   username: "wawa",
-   totalTopup: 1200000,
-   walletBalance: 410000
+   username: "jodai",
+   totalTopup: 715000,
+   walletBalance: 115000
  },
  {
-   username: "Coach Jack Dimitry",
-   totalTopup: 2345000,
-   walletBalance: 2120250
+   username: "sintanophe",
+   totalTopup: 1000000,
+   walletBalance: 53500
+ },
+ {
+   username: "nrlhasanah21",
+   totalTopup: 1000000,
+   walletBalance: 50000
+ },
+ {
+   username: "kiki",
+   totalTopup: 1550000,
+   walletBalance: 585250
+ },
+ {
+   username: "jasmine",
+   totalTopup: 1850000,
+   walletBalance: 103500
+ },
+ {
+   username: "nida",
+   totalTopup: 850000,
+   walletBalance: 175000
+ },
+ {
+   username: "sawung",
+   totalTopup: 250000,
+   walletBalance: 125000
+ },
+ {
+   username: "arin",
+   totalTopup: 850000,
+   walletBalance: 45000
+ },
+ {
+   username: "iiszakaria",
+   totalTopup: 450000,
+   walletBalance: 35000
+ },
+ {
+   username: "dinda",
+   "totalTopup": 780000,
+   "walletBalance": 135000
+ },
+ {
+   username: "sasaa",
+   "totalTopup": 1050000,
+   "walletBalance": 35000
+ },
+ {
+   username: "carol",
+   totalTopup: 400000,
+   walletBalance: 135000
+ },
+ {
+   username: "endah",
+   totalTopup: 600000,
+   walletBalance: 150000
+ },
+ {
+   username: "rarak",
+   totalTopup: 700000,
+   walletBalance: 290000
+ },
+ {
+   username: "rieke.dpz",
+   totalTopup: 300000,
+   walletBalance: 175000
+ },
+ {
+   username: "nafinida27",
+   totalTopup: 200000,
+   walletBalance: 75000
  }
 ];
 
