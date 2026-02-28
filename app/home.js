203bd382696694ec2e88c1f0f704450ebdbe2c68
maledannirh-design <male.dannirh@gmail.com>
2026-02-28 13:05:56 +0800

    Update home.js

--- app/home.js
+++ app/home.js
@@ -27,7 +27,7 @@ export async function renderHome(){
     const userData = userSnap.exists() ? userSnap.data() : {};
     const balance = userData.walletBalance || 0;
     const membership = userData.membership || "MEMBER"; // FIXED
-    const genre = userData.genre || "male";
+    const genre = userData.genre || "female";
 
     /* =============================
        MEMBER CARD LOOKUP
