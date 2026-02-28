2e60b9f34fe78d0bff4009b03191a9a25c6a58b9
maledannirh-design <male.dannirh@gmail.com>
2026-02-28 13:01:01 +0800

    Update utils.js

--- app/utils.js
+++ app/utils.js
@@ -10,3 +10,28 @@ export async function safeExecute(fn){
     showToast("System error");
   }
 }
+
+/* =============================
+   RESOLVE MEMBER CARD TEMPLATE
+============================= */
+export function resolveMemberCard(membership, genre){
+
+  const GREEN_CARD =
+    "https://raw.githubusercontent.com/maledannirh-design/giltclub-mobile/main/app/image/card/member_card.webp";
+
+  const PINK_CARD =
+    "https://raw.githubusercontent.com/maledannirh-design/giltclub-mobile/main/app/image/card/member_card_pink.webp";
+
+  const BLACK_CARD =
+    "https://raw.githubusercontent.com/maledannirh-design/giltclub-mobile/main/app/image/card/vvip_card.webp";
+
+  if(membership === "VVIP"){
+    return BLACK_CARD;
+  }
+
+  if(genre === "female"){
+    return PINK_CARD;
+  }
+
+  return GREEN_CARD;
+}
