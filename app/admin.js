331f22b59513aa84b95396871ad15291efe3a142
maledannirh-design <male.dannirh@gmail.com>
2026-03-03 02:59:36 +0800

    Update admin.js

--- app/admin.js
+++ app/admin.js
@@ -13,6 +13,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import { runMigration } from "./migration.js";
+import { renderFlashAdmin } from "./flash-admin.js";
 
 /* =====================================================
    RENDER ADMIN PANEL
