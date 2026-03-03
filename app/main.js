a1d51f95054dd3cf185a643eaa7e0673dc19e85d
maledannirh-design <male.dannirh@gmail.com>
2026-03-03 17:57:53 +0800

    Update main.js

--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ import { navigate } from "./navigation.js";
 import { auth, db } from "./firebase.js";
 import { initTheme, toggleTheme } from "./theme.js";
 import { showToast } from "./ui.js";
+import { initChatUnreadGlobal } from "./services/chatUnreadGlobal.js";
 
 import {
   doc,
