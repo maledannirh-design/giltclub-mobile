ab6112cb15ca0e406ba9ed296a431d0becd698ad
maledannirh-design <male.dannirh@gmail.com>
2026-03-03 18:40:57 +0800

    Update chat.js

--- app/chat.js
+++ app/chat.js
@@ -145,3 +145,17 @@ if(!activeRoomId){
     }
   );
 }
+
+function formatTime(date){
+  const now = new Date();
+  const isToday =
+    date.getDate() === now.getDate() &&
+    date.getMonth() === now.getMonth() &&
+    date.getFullYear() === now.getFullYear();
+
+  if(isToday){
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  }else{
+    return date.toLocaleDateString();
+  }
+}
