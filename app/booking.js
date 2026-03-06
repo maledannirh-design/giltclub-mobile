f06a610b46e806c7e54322b357dd405824053691
maledannirh-design <male.dannirh@gmail.com>
2026-03-06 22:00:02 +0800

    Update booking.js

--- app/booking.js
+++ app/booking.js
@@ -11,6 +11,7 @@ import {
   updateDoc,
   onSnapshot,
   addDoc,
+  Timestamp,
   runTransaction,   // ✅ TAMBAHKAN INI
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
