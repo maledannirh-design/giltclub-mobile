98e7b19ec069866ffecd2e73b62a4e990f796801
maledannirh-design <male.dannirh@gmail.com>
2026-03-07 11:32:06 +0800

    Update main.js

--- app/main.js
+++ app/main.js
@@ -29,8 +29,6 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { runVerifiedCheck } from "./verifiedlist/verifiedService.js";
-
 /* =========================================
    MAINTENANCE GUARD (PRO VERSION)
 ========================================= */
