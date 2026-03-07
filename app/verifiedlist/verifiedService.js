99cc960a24e07a43fd1384da8cea026977e11887
maledannirh-design <male.dannirh@gmail.com>
2026-03-07 11:45:39 +0800

    Update verifiedService.js

--- app/verifiedlist/verifiedService.js
+++ app/verifiedlist/verifiedService.js
@@ -1,15 +1,24 @@
 import { evaluateVerifiedStatus } from "./verifiedEngine.js";
 import { showVerifiedWarningPopup } from "./verifiedPopup.js";
 
-export function runVerifiedCheck(user) {
+export function runVerifiedCheck(user){
 
-  if (!user) return;
+  if(!user) return;
+
+  // ambil tanggal WITA
+  const now = new Date();
+  const witaDate = new Date(
+    now.toLocaleString("en-US", { timeZone: "Asia/Makassar" })
+  );
+
+  const day = witaDate.getDate();
+
+  // popup hanya tanggal 1 - 7 WITA
+  if(day < 1 || day > 7) return;
 
   const result = evaluateVerifiedStatus(user);
 
-  const today = new Date().getDate();
-
-  if (result.warning && today <= 7) {
+  if(result.warning){
     showVerifiedWarningPopup();
   }
 
