a540e5c7cb5dd5048f9285583189c072988cdf3c
maledannirh-design <male.dannirh@gmail.com>
2026-03-08 23:41:17 +0800

    Update userService.js

--- app/services/userService.js
+++ app/services/userService.js
@@ -51,3 +51,9 @@ export async function recordUserOnlineLog(){
   }
 
 }
+
+
+export async function getTopMonthlyUsers(month){
+   const ref = collection(db,"monthly_stats",month,"users");
+   return getDocs(query(ref,orderBy("attendance","desc"),limit(10)));
+}
